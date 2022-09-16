3e765b665ae622082fb6538fa07140d4eb02219a
090297anhduc <090297anhduc@gmail.com>
2022-09-16 18:05:33 +0700

    create repo

---
+++ src/test/java/com/ducnguyen/spoauth2/SpOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducnguyen.spoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
